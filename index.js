3a6c1c57138c388de045b8c32644185f0a9697fb
username <Ryan>
2023-02-13 12:01:47 -0600

    edit

--- index.js
+++ index.js
@@ -5,10 +5,6 @@ const searchResult = document.querySelector(".search__input");
 const movieList = document.querySelector(".top-picks");
 const loadState = document.querySelector(".loading");
 
-function clearResult() {
-  document.querySelector(".filter").value = "1";
-}
-
 async function onSearchChange(event) {
   const id = event.target.value;
   main(searchResult.value, id);
@@ -19,27 +15,30 @@ async function main(search, id) {
   let url = `https://omdbapi.com/?s=${search}&page=${id}&apikey=857ca5ce`;
   let res = await fetch(`${url}`);
   const data = await res.json();
+  // console.log(data);
   console.log(data);
   loadState.style.display = "inline";
 
   let moviesFilters = data.Search;
   for (let i = 0; i <= moviesFilters; i++) {
-    if (moviesFilters[i] != "game") {
+    if (moviesFilters[i] === "movie") {
       console.log(moviesFilters[i]);
     }
   }
+
   console.log(moviesFilters);
-  console.log(data);
-  movieList.innerHTML = data.Search.map((movies) => mvDataHTML(movies)).join(
-    ""
-  );
+  movieList.innerHTML = data.Search.map((movies) => {
+    // movie type and if there is no Poster, do not display
+    if (movies.Type === "movie" && movies.Poster !== "N/A") {
+      return mvDataHTML(movies);
+    }
+  }).join("");
 }
 
 function movieLoad() {
   return (window.onload = () => {
     document.querySelector(".btn").onclick = () => {
       main(searchResult.value);
-      clearResult();
     };
   });
 }
@@ -47,27 +46,15 @@ function movieLoad() {
 function mvDataHTML(movie) {
   return `<div class="movie__card">
   <figure class="poster__wrapper">
-    <img src = "${
-      movie.Poster != "N/A"
-        ? movie.Poster
-        : "https://www.salonlfc.com/wp-content/uploads/2018/01/image-not-found-scaled-1150x647.png"
-    }" alt = "movie poster"
+    <img src = "${movie.Poster}" alt = "movie poster"
       class="top-picks--img"
     />
   </figure>
   <h4 class="move__title">Title: ${movie.Title}</h4>
   <p class="movie__para">Year: ${movie.Year}</p>
-  <p class="movie__para">Type: ${movie.Type}</p>
+  <p class="movie__para">Type:  ${movie.Type}</p>
 
 </div>`;
 }
 
 movieLoad();
-
-// how reset option in html?
-
-{
-  /* <input id="result" type="text" />
-<input type="button" value="Reset" onclick="clearResult()">
- */
-}
